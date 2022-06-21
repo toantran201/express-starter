cc2593ba6d50e145d7706f60eea5b4efa1f021f9
toantran201 <toantran201.w@gmail.com>
2022-06-22 02:25:00 +0700

    Add process env port

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ app.delete('/api/notes/:id', (req, res) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
