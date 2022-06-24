5cf4c25e0e514e6cfc6f70c83be2f619ccd23f6d
toantran201 <toantran201.w@gmail.com>
2022-06-24 15:34:13 +0700

    Add lint

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'commonjs': true,
+        'es2021': true
+    },
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'ecmaVersion': 'latest'
+    },
+    'rules': {
+        'indent': [
+            'error',
+            2
+        ],
+        'linebreak-style': [
+            'error',
+            'windows'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ],
+        'object-curly-spacing': [
+            'error', 'always'
+        ],
+        'arrow-spacing': [
+            'error', { 'before': true, 'after': true }
+        ],
+        'no-console': 0
+    }
+}
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
 const express = require('express')
-const morgan = require("morgan")
+const morgan = require('morgan')
 const cors = require('cors')
-const Note = require("./model/note");
+const Note = require('./model/note')
 
 const app = express()
 app.use(express.json())
@@ -11,88 +11,88 @@ app.use(cors())
 app.use(express.static('dist'))
 
 app.get('/', (req, res) => {
-    res.send(`<h1>Welcome to hello-world app</h1>`)
+  res.send('<h1>Welcome to hello-world app</h1>')
 })
 
 // Notes
 app.get('/api/notes', (req, res) => {
-    Note.find({}).then(notes => {
-        res.json(notes)
-    }).catch(err => {
-        console.log({err})
-    })
+  Note.find({}).then(notes => {
+    res.json(notes)
+  }).catch(err => {
+    console.log({ err })
+  })
 })
 
 app.get('/api/notes/:id', (req, res, next) => {
-    Note.findById(req.params.id)
-        .then(note => {
-            if (note) {
-                res.json(note)
-            } else {
-                res.status(404).end()
-            }
-        })
-        .catch(error => next(error))
+  Note.findById(req.params.id)
+    .then(note => {
+      if (note) {
+        res.json(note)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/notes', (req, res, next) => {
-    const body = req.body
-    const note = new Note({
-        content: body.content,
-        important: body.important || false,
-        date: new Date(),
-    })
+  const body = req.body
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  })
 
-    note.save()
-        .then(savedNote => {
-            res.json(savedNote)
-        })
-        .catch(error => next(error))
+  note.save()
+    .then(savedNote => {
+      res.json(savedNote)
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (req, res, next) => {
-    const {content, important} = req.body
+  const { content, important } = req.body
 
 
-    Note.findByIdAndUpdate(
-        req.params.id,
-            {content, important},
-            {new: true, runValidators: true, context: 'query'}
-        )
-        .then(updatedNote => {
-            res.json(updatedNote)
-        })
-        .catch(error => next(error))
+  Note.findByIdAndUpdate(
+    req.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then(updatedNote => {
+      res.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (req, res, next) => {
-    Note.findByIdAndRemove(req.params.id)
-        .then(result => {
-            console.log(result)
-            res.status(204).end()
-        })
-        .catch(err => next(err))
+  Note.findByIdAndRemove(req.params.id)
+    .then(result => {
+      console.log(result)
+      res.status(204).end()
+    })
+    .catch(err => next(err))
 })
 
 // handler of requests with unknown endpoint
 const unknownEndpoint = (req, res) => {
-    res.status(404).send({error: 'unknown endpoint'})
+  res.status(404).send({ error: 'unknown endpoint' })
 }
 app.use(unknownEndpoint)
 
 // Error handler middleware - should be the last loaded middleware
 const errorHandler = (error, req, res, next) => {
-    console.error(error)
+  console.error(error)
 
-    switch (error.name){
-        case 'CastError': return res.status(400).send({error: 'malformatted id'})
-        case 'ValidationError': return res.status(400).json({error: error.message})
-    }
-    next(error)
+  switch (error.name){
+  case 'CastError': return res.status(400).send({ error: 'malformatted id' })
+  case 'ValidationError': return res.status(400).json({ error: error.message })
+  }
+  next(error)
 }
 app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`)
 })
