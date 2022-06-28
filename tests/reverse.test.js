fdd7d24f1e1f3cd475c7bdfb40581f016c649211
toantran201 <toantran201.w@gmail.com>
2022-06-29 00:13:50 +0700

    Add testing BE env

---
+++ tests/reverse.test.js
@@ -0,0 +1,18 @@
+const reverse = require("../utils/for-testing").reverse
+
+test('Reverse of a', () => {
+    const result = reverse('a')
+    expect(result).toBe('a')
+})
+
+test('reverse of react', () => {
+    const result = reverse('react')
+
+    expect(result).toBe('tcaer')
+})
+
+test('reverse of releveler', () => {
+    const result = reverse('releveler')
+
+    expect(result).toBe('releveler')
+})
